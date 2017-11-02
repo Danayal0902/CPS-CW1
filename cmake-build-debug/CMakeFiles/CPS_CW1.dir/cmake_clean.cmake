99d595e5964b602ba1da7c28835f6280320ef799
Danayal Iftikhar <danayal_94@hotmail.com>
2017-11-02 18:15:31 +0000

    Initial Commit
    cba

---
+++ cmake-build-debug/CMakeFiles/CPS_CW1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPS_CW1.dir/main.cpp.o"
+  "CPS_CW1.pdb"
+  "CPS_CW1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPS_CW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
